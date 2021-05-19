8ccfaf8727fe900c08689f3515794e24fcd5136c
Glenio Carvalho <gleniohenrique.florindo@q2pay.com.br>
2021-05-19 17:28:07 -0300

    arrays

---
+++ array/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var teste [3]int
+	teste[0] = 3
+	teste[1] = 2
+	teste[2] = 1
+	fmt.Println("Qual a capacidade deste array? ", len(teste))
+	cantores := [2]string{"Michael Jackson", "Jhon Lennon"}
+	fmt.Printf("O que há nesse array? \n\r%v\r\n", cantores)
+
+	capitais := [...]string{"Lisboa", "Brasilia", "Luanda", "Maputo"}
+	fmt.Println("Qual a capacidade deste array? ", len(capitais))
+	for indice, cidade := range capitais {
+		fmt.Printf("Capital[%d] é %s\n\r", indice, cidade)
+	}
+}
